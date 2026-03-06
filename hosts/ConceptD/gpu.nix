2c8c903ebc05c91f0a5952951e3ea73529496cfe
akerka2 <akerka@gmail.com>
2026-03-07 00:24:41 +0200

    ConceptD: addPRIME config for GTX 1650 + Intel

--- hosts/ConceptD/gpu.nix
+++ hosts/ConceptD/gpu.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
-  # Nvidia GeForce GTX 1050
+  # Nvidia GeForce GTX 1650 Max-Q + intel PRIME
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
@@ -8,6 +8,11 @@
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+      sync.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true; # For Steam
