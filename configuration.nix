16fb854e5e66197ddb8e8c652922d289a08c15fc
akerka2 <akerka@gmail.com>
2026-08-20 15:20:59 +0300

    Work on docker

--- configuration.nix
+++ configuration.nix
@@ -142,8 +142,8 @@ in
 
   environment.systemPackages = with pkgs; [
     autoPatchelfHook # Патчить бинарные файлы для работы в условиях nixos
-    #pkgsRocm.blender # Blender with HIP support
-    blenderHipFixed
+    pkgsRocm.blender # Blender with HIP support
+    #blenderHipFixed
     cage # Run gui-apps in tty by: cage _programname_
     direnv # For vs code nixos edits
     dracut # Provides lsinitrd
